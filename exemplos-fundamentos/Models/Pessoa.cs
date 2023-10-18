1fa7e2f696b9c686faa4a5b373f02c0a27c46d7a
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-10-18 16:17:17 -0300

    Primeiros código em c#

---
+++ exemplos-fundamentos/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using exemplos_fundamentos.Models;
+
+namespace exemplos_fundamentos.Models
+{
+    public class Pessoa
+    {
+       public string Nome { get; set; }
+       public int Idade { get; set; } 
+
+       public void Apresentar()
+       {
+         Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+       }
+    }
+}
