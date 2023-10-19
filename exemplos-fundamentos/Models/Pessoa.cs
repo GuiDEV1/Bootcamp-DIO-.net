52c099cd6e0c59d39e49bd9f9694735bb1fa0bc0
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-10-19 18:02:05 -0300

    sintaxe e tipos de dados

--- exemplos-fundamentos/Models/Pessoa.cs
+++ exemplos-fundamentos/Models/Pessoa.cs
@@ -8,12 +8,13 @@ namespace exemplos_fundamentos.Models
 {
     public class Pessoa
     {
-       public string Nome { get; set; }
+       public string? Nome { get; set; }
        public int Idade { get; set; } 
+       public string? NomeRepresentanteLegal { get; set; } 
 
        public void Apresentar()
        {
-         Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+         Console.WriteLine($"Olá, meu nome é {Nome}, \n e tenho {Idade} anos");
        }
     }
 }
--- exemplos-fundamentos/Program.cs
+++ exemplos-fundamentos/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using exemplos_fundamentos.Models;
+
+// string apresentacao = "Olá, seja bem vindo!!";
+
+// int quantidade = 1;
+// quantidade = 34;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool mundialDoPalmeiras = false;
+
+// Console.WriteLine("O valor da variavel apresentação: " + apresentacao);
+// Console.WriteLine("O valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("O valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("O valor da variavel Preco: " + preco);
+// Console.WriteLine("O valor da variavel mundialDoPalmeiras: " + mundialDoPalmeiras);
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Guilherme";
+// pessoa1.Idade = 22;
+// pessoa1.Apresentar();
