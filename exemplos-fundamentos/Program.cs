3f648c982fa2c568e0dbc3a522286a948c9c87dc
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-10-31 21:14:47 -0300

    add content

--- exemplos-fundamentos/Program.cs
+++ exemplos-fundamentos/Program.cs
@@ -14,17 +14,45 @@
 // Console.WriteLine("O valor da variavel Preco: " + preco);
 // Console.WriteLine("O valor da variavel mundialDoPalmeiras: " + mundialDoPalmeiras);
 
-DateTime dataAtual = DateTime.Now.AddDays(5);
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
+// int a = 10 , b = 20;
+// int c = a + b;
+// c *= 5;
 
+// Console.WriteLine($"O total da váriavel c é: {c}");
 
+// // Cast - Casting
+// int a = Convert.ToInt32(null);
+// //int a = int.Parse(null); 
 
+// Console.WriteLine(a); 
 
+// int inteiro = 5;
+// string a = inteiro.ToString();
+// Console.WriteLine(a);
 
+// int a = 5;
+// double b = a;
 
+// long a = long.MaxValue;
+// int b = Convert.ToInt32(a);
 
+// int a = int.MaxValue;
+// long b = a;
+// Console.WriteLine(b);
 
+// double a = 4 / (2 + 2);
+
+// Console.WriteLine(a);
+
+string a = "15A";
+int b = 0;
+
+int.TryParse(a, out b);
+
+Console.WriteLine(b);
 
 
 
