3c0a246c010f0b6490d584bbe9a71c57b64ae4f1
Guilherme <guilhermemascarenhas21g@gmail.com>
2023-11-01 21:09:43 -0300

    add content

--- exemplos-fundamentos/Program.cs
+++ exemplos-fundamentos/Program.cs
@@ -1,4 +1,4 @@
-﻿// using exemplos_fundamentos.Models;
+﻿ using exemplos_fundamentos.Models;
 
 // string apresentacao = "Olá, seja bem vindo!!";
 
@@ -47,27 +47,77 @@
 
 // Console.WriteLine(a);
 
-string a = "15A";
-int b = 0;
+// string a = "15A";
+// int b = 0;
 
-int.TryParse(a, out b);
+// int.TryParse(a, out b);
 
-Console.WriteLine(b);
-
-
-
-
-
-
-
-
-
-
-
-
+// Console.WriteLine(b);
 
 // Pessoa pessoa1 = new Pessoa();
 
 // pessoa1.Nome = "Guilherme";
 // pessoa1.Idade = 22;
 // pessoa1.Apresentar();
+
+// int quantidadeEstoque = 10, quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+// Console.WriteLine($"Quantidade em compra: {quantidadeCompra}");
+// Console.WriteLine($"É possivel realizar a venda ? {possivelVenda}");
+
+// if (quantidadeCompra == 0) {
+//     Console.WriteLine("Venda invalida");
+// }
+// else if (possivelVenda) {
+//    Console.WriteLine("Venda realizada.");
+// }
+// else {
+//     Console.WriteLine("Desculpe a quantidade que deseja comprar não estar disponivel");
+// }
+
+
+// Console.WriteLine("Digite uma letra");
+// string  letra = Console.ReadLine();
+
+// switch (letra) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//       Console.WriteLine("Vogal");
+//       break;
+//     default:
+//       Console.WriteLine("Não é uma Vogal");
+//       break;
+// }
+
+// bool eMaiorDeIdade = true;
+// bool possuiAutorizacaoDoResposavel = false;
+
+// if(eMaiorDeIdade || possuiAutorizacaoDoResposavel) {
+//  Console.WriteLine("Entrada liberada!");
+// } else {
+//   Console.WriteLine("Entrada não liberada");
+// }
+
+// bool possuiPresencaMinima = false;
+// double media = 7.5;
+
+// if(possuiPresencaMinima && media >= 7) {
+//   Console.WriteLine("Aprovado");
+// } else {
+//   Console.WriteLine("Reprovado");
+// }
+
+bool choveu = false;
+bool estaTarde = false;
+
+if(!choveu && !estaTarde) {
+  Console.WriteLine("Vou pedalar");
+} else {
+   Console.WriteLine("Vou pedalar outro dia!");
+}
+
